8f282b621308c6c029ad8e87344c9c7a50054349
Cherry <jd621094@hotmail.com>
2024-11-06 03:26:04 -0300

    Se agrego la pagina productos

--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductosService } from './../servicios/producto/productos.service'
+import { Producto } from './../interfaces/Producto';
+import { ViewWillEnter, ViewDidLeave } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.page.html',
   styleUrls: ['./productos.page.scss'],
 })
-export class ProductosPage implements OnInit {
+export class ProductosPage implements ViewWillEnter,ViewDidLeave  {
+  public productos: Producto[] = [];
+  private subProucto!: Subscription;
+  constructor(
+    private prdS: ProductosService,
+  ) {
 
-  constructor() { }
+  }
 
-  ngOnInit() {
+  ionViewDidLeave(): void {
+    if(this.subProucto){
+      this.subProucto.unsubscribe();
+    }
+  }
+
+  ionViewWillEnter(): void {
+    this.subProucto = this.prdS.producto.subscribe(productos => {
+      this.productos = productos;
+    });
+    this.prdS.listarProductos();
   }
 
 }
